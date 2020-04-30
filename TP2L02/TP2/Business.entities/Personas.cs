e94a4555290cfde15b55781e5929579dfb38f4f3
Juan Barrientos <assacreed100@outlook.com>
2020-04-30 17:52:23 -0300

    Fin de aprendizaje

--- TP2L02/TP2/Business.entities/Personas.cs
+++ TP2L02/TP2/Business.entities/Personas.cs
@@ -59,13 +59,7 @@ namespace BusinessEntity
         public enum TipoPersona
         {
             Alumno,
-            Docente,
-            jefeCatedra,
-            papa,
-            queso,
-            pimiento,
-            lapapota,
-            lapapota2
+            Docente
 
         }
 
