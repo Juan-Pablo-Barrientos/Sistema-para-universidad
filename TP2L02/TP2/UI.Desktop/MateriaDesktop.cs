dbf30db375f57160ad510270c2c5ed042d5c4446
martisebastiang@gmail.com <martisebastiang@gmail.com>
2020-07-06 19:02:03 -0300

    MateriaDesktop avance

--- TP2L02/TP2/UI.Desktop/MateriaDesktop.cs
+++ TP2L02/TP2/UI.Desktop/MateriaDesktop.cs
@@ -39,6 +39,117 @@ namespace UI.Desktop
         }
         #endregion
 
+
+
+        public override void MapearDeDatos()
+        {
+            this.txtIdmateria.Text = this.MateriaActual.ID.ToString();      
+            this.txtIdplan.Text = this.MateriaActual.IDPlan.ToString();
+            this.txtDescripcion.Text = this.MateriaActual.Descripcion;
+            this.txtHssemanales.Text = this.MateriaActual.HSSemanales.ToString();
+            this.txtHstotales.Text = this.MateriaActual.HSTotales.ToString();
+            
+
+            if (Modo == ModoForm.Alta || Modo == ModoForm.Modificacion)
+            {
+                this.btnAceptar.Text = "Guardar";
+            }
+            else if (Modo == ModoForm.Baja)
+            {
+                this.btnAceptar.Text = "Eliminar";
+            }
+            else if (Modo == ModoForm.Consulta)
+            {
+                this.btnAceptar.Text = "Aceptar";
+            }
+
+        }
+   /*     public override void MapearADatos()
+        {
+
+            if (Modo == ModoForm.Alta)
+            {
+                MateriaActual = new Materia();
+            }
+            if (Modo == ModoForm.Alta || Modo == ModoForm.Modificacion)
+            {
+                MateriaActual.Nombre = txtNombre.Text;
+                MateriaActual.Apellido = txtApellido.Text;
+                MateriaActual.EMail = txtEmail.Text;
+                MateriaActual.NombreUsuario = txtUsuario.Text;
+                MateriaActual.Clave = txtClave.Text;
+                MateriaActual.Clave = txtConfirmarClave.Text;
+                MateriaActual.Habilitado = chkHabilitado.Checked;
+
+                switch (Modo)
+                {
+                    case ModoForm.Alta:
+                        {
+                            MateriaActual.State = BusinessEntity.States.New;
+                            break;
+                        }
+                    case ModoForm.Modificacion:
+                        {
+                            MateriaActual.State = BusinessEntity.States.Modified;
+                            break;
+                        }
+                    case ModoForm.Consulta:
+                        {
+                            MateriaActual.State = BusinessEntity.States.Unmodified;
+                            break;
+                        }
+                    case ModoForm.Baja:
+                        {
+                            MateriaActual.State = BusinessEntity.States.Deleted;
+                            break;
+                        }
+                }
+            }
+        }
+        public override void GuardarCambios()
+        {
+            MapearADatos();
+            new UsuarioLogic().Save(MateriaActual);
+        }
+        public override bool Validar()
+        {
+
+            // Validar que los campos no esten vacios 
+
+            foreach (Control oControls in this.Controls) // Buscamos en cada TextBox de nuestro Formulario.
+            {
+                if (oControls is TextBox & oControls.Text == String.Empty) // Verificamos que no este vacio.
+                {
+                    Notificar("Hay al menos un campo vacío. Por favor, completelo/s. ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return (false);
+                }
+            }
+
+            //Validar el interior de los campos 
+
+            if (txtClave.Text != txtConfirmarClave.Text)
+            {
+                Notificar("La clave ingresada no coincide con la clave de confirmación. ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return (false);
+            }
+            else if (txtClave.Text.Length < 8)
+            {
+                Notificar("La clave ingresada debe ser al menos de 8 carateres de longitud.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return (false);
+            }
+
+            if (Business.Logic.Validar.EsMailValido(txtEmail.Text.Trim()))
+            {
+                Notificar("El email ingresado no es válido. ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return (false);
+            }
+
+            return (true);
+
+        }
+
+    */
+
         private void btnAceptar_Click(object sender, EventArgs e)
         {
 
@@ -46,12 +157,12 @@ namespace UI.Desktop
 
         private void btnCancelar_Click(object sender, EventArgs e)
         {
-
+            Close();
         }
 
         private void MateriaDesktop_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
