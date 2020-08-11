21894806fb9bff9f9af9e288a44ba45a5aaf20ea
martisebastiang@gmail.com <martisebastiang@gmail.com>
2020-08-11 11:47:55 -0300

    PlanDesktop (comentado partes no hechas)

---
+++ TP2L02/TP2/UI.Desktop/PlanDesktop.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace UI.Desktop
+{
+    partial class PlanDesktop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.container = new System.Windows.Forms.TableLayoutPanel();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.labelideplan = new System.Windows.Forms.Label();
+            this.labeldesc = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.container.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // container
+            // 
+            this.container.ColumnCount = 4;
+            this.container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.container.Controls.Add(this.btnCancelar, 3, 2);
+            this.container.Controls.Add(this.btnAceptar, 2, 2);
+            this.container.Controls.Add(this.txtID, 1, 0);
+            this.container.Controls.Add(this.labelideplan, 0, 0);
+            this.container.Controls.Add(this.txtDescripcion, 3, 0);
+            this.container.Controls.Add(this.labeldesc, 2, 0);
+            this.container.Controls.Add(this.label3, 0, 1);
+            this.container.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.container.Location = new System.Drawing.Point(0, 0);
+            this.container.Name = "container";
+            this.container.RowCount = 3;
+            this.container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.container.Size = new System.Drawing.Size(438, 82);
+            this.container.TabIndex = 0;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(221, 57);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 22);
+            this.btnAceptar.TabIndex = 0;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(330, 57);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 22);
+            this.btnCancelar.TabIndex = 1;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtDescripcion.Location = new System.Drawing.Point(330, 3);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(105, 20);
+            this.txtDescripcion.TabIndex = 2;
+            // 
+            // txtID
+            // 
+            this.txtID.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtID.Location = new System.Drawing.Point(112, 3);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(103, 20);
+            this.txtID.TabIndex = 3;
+            // 
+            // labelideplan
+            // 
+            this.labelideplan.AutoSize = true;
+            this.labelideplan.Location = new System.Drawing.Point(3, 0);
+            this.labelideplan.Name = "labelideplan";
+            this.labelideplan.Size = new System.Drawing.Size(57, 13);
+            this.labelideplan.TabIndex = 4;
+            this.labelideplan.Text = "ID de Plan";
+            // 
+            // labeldesc
+            // 
+            this.labeldesc.AutoSize = true;
+            this.labeldesc.Location = new System.Drawing.Point(221, 0);
+            this.labeldesc.Name = "labeldesc";
+            this.labeldesc.Size = new System.Drawing.Size(63, 13);
+            this.labeldesc.TabIndex = 5;
+            this.labeldesc.Text = "Descripcion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ID de especialidad";
+            // 
+            // PlanDesktop
+            // 
+            this.AcceptButton = this.btnAceptar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 82);
+            this.Controls.Add(this.container);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "PlanDesktop";
+            this.Text = "PlanDesktop";
+            this.container.ResumeLayout(false);
+            this.container.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel container;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label labelideplan;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label labeldesc;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ TP2L02/TP2/UI.Desktop/PlanDesktop.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
+using Util.entities;
+
+namespace UI.Desktop
+{
+    public partial class PlanDesktop : ApplicationForm
+    {
+
+        public Plan PlanActual;
+
+        #region constructores
+        public PlanDesktop()
+        {
+            InitializeComponent();
+            this.btnAceptar.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+
+        public PlanDesktop(ModoForm modo) : this()
+        {
+            Modo = modo;
+        }
+
+
+        public PlanDesktop(int ID, ModoForm modo) : this()
+        {
+            Modo = modo;
+            //PlanActual = new PlanesLogic().getOne(ID);
+            MapearDeDatos();
+
+        }
+        #endregion
+        
+
+        public override void MapearDeDatos()
+        {
+            this.txtID.Text = this.PlanActual.ID.ToString();
+            this.txtDescripcion.Text = this.PlanActual.Descripcion;
+
+            if (Modo == ModoForm.Alta || Modo == ModoForm.Modificacion)
+            {
+                this.btnAceptar.Text = "Guardar";
+            }
+            else if (Modo == ModoForm.Baja)
+            {
+                this.btnAceptar.Text = "Eliminar";
+            }
+            else if (Modo == ModoForm.Consulta)
+            {
+                this.btnAceptar.Text = "Aceptar";
+            }
+
+        }
+        public override void MapearADatos()
+        {
+
+            if (Modo == ModoForm.Alta)
+            {
+                PlanActual = new Plan();
+            }
+            if (Modo == ModoForm.Alta || Modo == ModoForm.Modificacion)
+            {
+                PlanActual.Descripcion = txtDescripcion.Text;
+
+                switch (Modo)
+                {
+                    case ModoForm.Alta:
+                        {
+                            PlanActual.State = BusinessEntity.States.New;
+                            break;
+                        }
+                    case ModoForm.Modificacion:
+                        {
+                            PlanActual.State = BusinessEntity.States.Modified;
+                            break;
+                        }
+                    case ModoForm.Consulta:
+                        {
+                            PlanActual.State = BusinessEntity.States.Unmodified;
+                            break;
+                        }
+                    case ModoForm.Baja:
+                        {
+                            PlanActual.State = BusinessEntity.States.Deleted;
+                            break;
+                        }
+                }
+            }
+        }
+        public override void GuardarCambios()
+        {
+            MapearADatos();
+            //new PlanesLogic().Save(PlanActual);
+        }
+        public override bool Validar()
+        {
+            var validador = new Validador();
+            List<string> Campos = (this.container.Controls.OfType<TextBox>().Where(txt => txt.ReadOnly == false).Select(txt => txt.Text)).ToList();
+            if (!BusinessLogic.SonCamposValidos(Campos)) validador.AgregarError("No todos los campos estan completos");
+            if (!validador.EsValido()) BusinessLogic.Notificar("Plan", validador.Errores, MessageBoxButtons.OK, MessageBoxIcon.Error);//Si no es valido, mustra el error
+            return validador.EsValido();
+        }
+
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (Validar())
+            {
+                GuardarCambios();
+
+                Close();
+            }
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void PlanDesktop_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
