7c52e8182555a795fe87a2c21c427fb46417c5df
martisebastiang@gmail.com <martisebastiang@gmail.com>
2020-08-17 17:21:19 -0300

    ComisionDesktop (2 comentarios por la sentencia ComisionLogic)

---
+++ TP2L02/TP2/UI.Desktop/ComisionDesktop.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace UI.Desktop
+{
+    partial class ComisionDesktop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.container = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtIdComision = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.cBIdPlan = new System.Windows.Forms.ComboBox();
+            this.txtAnio = new System.Windows.Forms.TextBox();
+            this.container.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // container
+            // 
+            this.container.ColumnCount = 4;
+            this.container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.container.Controls.Add(this.label1, 0, 0);
+            this.container.Controls.Add(this.label3, 2, 0);
+            this.container.Controls.Add(this.label2, 0, 1);
+            this.container.Controls.Add(this.label4, 2, 1);
+            this.container.Controls.Add(this.txtIdComision, 1, 0);
+            this.container.Controls.Add(this.txtDescripcion, 3, 0);
+            this.container.Controls.Add(this.btnCancelar, 3, 2);
+            this.container.Controls.Add(this.btnAceptar, 2, 2);
+            this.container.Controls.Add(this.cBIdPlan, 1, 1);
+            this.container.Controls.Add(this.txtAnio, 3, 1);
+            this.container.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.container.Location = new System.Drawing.Point(0, 0);
+            this.container.Name = "container";
+            this.container.RowCount = 3;
+            this.container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.container.Size = new System.Drawing.Size(438, 89);
+            this.container.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID comision";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Descripcion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Plan";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(221, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Año ";
+            // 
+            // txtIdComision
+            // 
+            this.txtIdComision.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtIdComision.Location = new System.Drawing.Point(112, 3);
+            this.txtIdComision.Name = "txtIdComision";
+            this.txtIdComision.Size = new System.Drawing.Size(103, 20);
+            this.txtIdComision.TabIndex = 4;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtDescripcion.Location = new System.Drawing.Point(330, 3);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(105, 20);
+            this.txtDescripcion.TabIndex = 5;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(330, 61);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 9;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(221, 61);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 8;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // cBIdPlan
+            // 
+            this.cBIdPlan.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.cBIdPlan.FormattingEnabled = true;
+            this.cBIdPlan.Location = new System.Drawing.Point(112, 32);
+            this.cBIdPlan.Name = "cBIdPlan";
+            this.cBIdPlan.Size = new System.Drawing.Size(103, 21);
+            this.cBIdPlan.TabIndex = 10;
+            // 
+            // txtAnio
+            // 
+            this.txtAnio.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtAnio.Location = new System.Drawing.Point(330, 32);
+            this.txtAnio.Name = "txtAnio";
+            this.txtAnio.Size = new System.Drawing.Size(105, 20);
+            this.txtAnio.TabIndex = 6;
+            this.txtAnio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SoloNumeros_KeyPress);
+            // 
+            // ComisionDesktop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 89);
+            this.Controls.Add(this.container);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "ComisionDesktop";
+            this.Text = "ComisionDesktop";
+            this.container.ResumeLayout(false);
+            this.container.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel container;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtIdComision;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.ComboBox cBIdPlan;
+        private System.Windows.Forms.TextBox txtAnio;
+    }
+}
---
+++ TP2L02/TP2/UI.Desktop/ComisionDesktop.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
+using Util.entities;
+
+namespace UI.Desktop
+{
+    public partial class ComisionDesktop : ApplicationForm
+    {
+        public Comision ComisionActual;
+        List<Plan> Planes = new PlanLogic().GetAll();
+        #region constructores
+        public ComisionDesktop()
+        {
+            InitializeComponent();
+            this.btnAceptar.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            // Mostrar listado de Planes.                 
+            foreach (var p in Planes)
+            {
+                cBIdPlan.Items.Add(p.Descripcion);
+            }
+        }
+
+        public ComisionDesktop(ModoForm modo) : this()
+        {
+            Modo = modo;
+        }
+
+
+        public ComisionDesktop(int ID, ModoForm modo) : this()
+        {
+            Modo = modo;
+        //    ComisionActual = new ComisionLogic().getOne(ID);
+            MapearDeDatos();
+        }
+        #endregion
+
+
+
+        public override void MapearDeDatos()
+        {
+            this.txtIdComision.Text = this.ComisionActual.ID.ToString();
+            this.txtDescripcion.Text = this.ComisionActual.Descripcion;
+            this.txtAnio.Text = this.ComisionActual.AnioEspecialidad.ToString();          
+            foreach (var p in Planes.Where(p => p.ID == ComisionActual.IDPlan))
+            {
+                this.cBIdPlan.Text = p.Descripcion;
+            }
+
+            if (Modo == ModoForm.Alta || Modo == ModoForm.Modificacion)
+            {
+                this.btnAceptar.Text = "Guardar";
+            }
+            if (Modo == ModoForm.Baja)
+            {
+                this.btnAceptar.Text = "Eliminar";
+            }
+            if (Modo == ModoForm.Consulta)
+            {
+                this.btnAceptar.Text = "Aceptar";
+            }
+
+        }
+        public override void MapearADatos()
+        {
+
+            if (Modo == ModoForm.Alta)
+            {
+                ComisionActual = new Comision();
+            }
+            if (Modo == ModoForm.Alta || Modo == ModoForm.Modificacion)
+            {
+                ComisionActual.Descripcion = txtDescripcion.Text;
+                ComisionActual.AnioEspecialidad = Convert.ToInt32(txtAnio.Text);               
+                foreach (var p in Planes.Where(p => p.Descripcion == cBIdPlan.Text))
+                {
+                    ComisionActual.IDPlan = p.ID;
+                }
+
+                switch (Modo)
+                {
+                    case ModoForm.Alta:
+                        {
+                            ComisionActual.State = BusinessEntity.States.New;
+                            break;
+                        }
+                    case ModoForm.Modificacion:
+                        {
+                            ComisionActual.State = BusinessEntity.States.Modified;
+                            break;
+                        }
+                    case ModoForm.Consulta:
+                        {
+                            ComisionActual.State = BusinessEntity.States.Unmodified;
+                            break;
+                        }
+                    case ModoForm.Baja:
+                        {
+                            ComisionActual.State = BusinessEntity.States.Deleted;
+                            break;
+                        }
+                }
+            }
+        }
+        public override void GuardarCambios()
+        {
+            MapearADatos();
+           // new ComisionLogic().Save(ComisionActual);
+        }
+        public override bool Validar()
+        {
+            var validador = new Validador();
+            List<string> Campos = (this.container.Controls.OfType<TextBox>().Where(txt => txt.ReadOnly == false).Select(txt => txt.Text)).ToList();
+            if (!BusinessLogic.SonCamposValidos(Campos)) validador.AgregarError("No todos los campos estan completos");
+            if (cBIdPlan.SelectedItem == null) validador.AgregarError("Elija una especialidad");
+            if (!validador.EsValido()) BusinessLogic.Notificar("Comision", validador.Errores, MessageBoxButtons.OK, MessageBoxIcon.Error);//Si no es valido, mustra el error
+            return validador.EsValido();
+        }
+
+
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (Validar())
+            {
+                GuardarCambios();
+
+                Close();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ComisionDesktop_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Este metodo esta linkeado con el evento KeyPress, no permite que se ingrese otro caracter que no sea numerico
+        //Lo TextBox para la hora lo usan.
+        private void SoloNumeros_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.')) e.Handled = true;
+        }
+    }
+}
